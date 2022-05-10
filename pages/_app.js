4e5c2b57bfbc021d6aa2169d91b038abc9ee85aa
developjik <developjik@gmail.com>
2022-05-10 22:10:41 +0900

    feat: next-auth 일반 로그인 및 소셜 로그인 기능 구현

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
+import { SessionProvider } from 'next-auth/react';
+
+import emotionReset from 'emotion-reset';
+import { Global, css } from '@emotion/react';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Global styles={GLOBAL_STYLE} />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+const GLOBAL_STYLE = css`
+  ${emotionReset}
+
+  *, *::after, *::before {
+    box-sizing: border-box;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    font-smoothing: antialiased;
+  }
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+`;
+
+export default MyApp;
